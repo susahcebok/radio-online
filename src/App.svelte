4a91b508d6006831e74d642706abb661f80bce07
Ferry Ayunda <115649359+susahcebok@users.noreply.github.com>
2022-11-03 13:04:22 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -15,7 +15,7 @@
 
 <div id="sun"></div>
 
-<div id="title">Radio {name} FM</div>
+<div id="title">Music {name} FM</div>
 <button id="button" onclick="play()" title="Don't spam click it, it would play eventually">CLICK TO PLAY MUSIC</button>
 
 
